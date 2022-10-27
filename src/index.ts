fb68bee8d6d70184f1cff27a68b1e71fb045481e
Brent Shambaugh <brent.shambaugh@gmail.com>
2022-10-27 15:09:07 -0500

    back up state for P256Class, it's not tested ... nothing is guaranteed

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import { createJWS, decryptJWE, ES256Signer } from 'did-jwt'
+import { createJWS, ES256Signer } from 'did-jwt'
 // import { ecdhEsA256KwDecrypter } from 'did-jwt' // did-jwt/src/encrypters/ecdhEsA256Kw.ts
-import { HandlerMethods, RequestHandler, RPCConnection, RPCError, RPCRequest, RPCResponse, createHandler, SendRequestFunc } from 'rpc-utils'
-import type { AuthParams, CreateJWSParams, DecryptJWEParams, DIDMethodName, DIDProviderMethods, DIDProvider, GeneralJWS } from 'dids'
+import { HandlerMethods, RPCError, RPCRequest, RPCResponse, createHandler, SendRequestFunc } from 'rpc-utils'
+import type { AuthParams, CreateJWSParams, DIDMethodName, DIDProviderMethods, DIDProvider, GeneralJWS } from 'dids'
 import stringify from 'fast-json-stable-stringify'
 import * as u8a from 'uint8arrays'
 import { ec as EC } from 'elliptic'
+import { encodeDIDfromHexString } from 'did-key-creator'
 
-const B64 = 'base64pad'
+//const B64 = 'base64pad'
 
 const ec = new EC('p256')
 
@@ -27,7 +28,7 @@ function toGeneralJWS(jws: string): GeneralJWS {
 
 interface Context {
   did: string
-  secretKey: Uint8Array
+  privateKey: Uint8Array
 }
 
 
@@ -39,13 +40,14 @@ const sign = async (
 ) => {
   const kid = `${did}#${did.split(':')[2]}`
   // need remote signer here as well: https://github.com/decentralized-identity/did-jwt/blob/cebf2e6f255e559a1275bb97b35146ce72ce27f5/docs/guides/index.md#creating-custom-signers-for-integrating-with-hsm
-  const signer = ES256Signer(u8a.toString(secretKey, B64)) // look at did-jwt tests to find what ES256Signer requires
-  const header = toStableObject(Object.assign(protectedHeader, { kid, alg: 'EdDSA' }))
+  //const signer = ES256Signer(u8a.toString(secretKey, B64)) // look at did-jwt tests to find what ES256Signer requires
+  const signer = ES256Signer(secretKey)
+  const header = toStableObject(Object.assign(protectedHeader, { kid, alg: 'ES256' }))
   return createJWS(typeof payload === 'string' ? payload : toStableObject(payload), signer, header)
 }
 
 const didMethods: HandlerMethods<Context, DIDProviderMethods> = {
-  did_authenticate: async ({ did, secretKey }, params: AuthParams) => {
+  did_authenticate: async ({ did, privateKey }, params: AuthParams) => {
     const response = await sign(
       {
         did,
@@ -55,20 +57,22 @@ const didMethods: HandlerMethods<Context, DIDProviderMethods> = {
         exp: Math.floor(Date.now() / 1000) + 600, // expires 10 min from now
       },
       did,
-      secretKey
+      privateKey
     )
     return toGeneralJWS(response)
   },
-  did_createJWS: async ({ did, secretKey }, params: CreateJWSParams & { did: string }) => {
+  did_createJWS: async ({ did, privateKey }, params: CreateJWSParams & { did: string }) => {
     const requestDid = params.did.split('#')[0]
     if (requestDid !== did) throw new RPCError(4100, `Unknown DID: ${did}`)
-    const jws = await sign(params.payload, did, secretKey, params.protected)
+    const jws = await sign(params.payload, did, privateKey, params.protected)
     return { jws: toGeneralJWS(jws) }
   },
-  did_decryptJWE: async ({ secretKey }, params: DecryptJWEParams) => {
+  did_decryptJWE: async () => {
      // this needs to be implemented in the did-jwt library
      // const decrypter = ecdhEsA256KwDecrypter(params.jwe, decrypter)
      // instead of the 4100 Error, emulate the commented out code below...
+     // this will be able to change when did-jwt supports ES256 for JWE 
+     // https://github.com/decentralized-identity/did-jwt/issues/225
      throw new RPCError(4100, 'Decryption not supported')
   },
   /*
@@ -87,15 +91,17 @@ const didMethods: HandlerMethods<Context, DIDProviderMethods> = {
 export class P256Provider implements DIDProvider {
   _handle: SendRequestFunc<DIDProviderMethods>
 
-  constructor(seed: Uint8Array) {
+  constructor() {
     // just use the library elliptic to do this...
     const kp = ec.genKeyPair();
     // const { secretKey, publicKey } = generateKeyPairFromSeed(seed)
     const publicKey = String(kp.getPublic('hex'))
+    //const publicKey = kp.getPublic()
     const secretKey = String(kp.getPrivate('hex'))
-    const did = encodeDID(publicKey) // replace with encodeDIDfromBytes from did-key-creator
+    const privateKey = u8a.fromString(secretKey,'hex')
+    const did = encodeDIDfromHexString('p256-pub',publicKey) // replace with encodeDIDfromBytes from did-key-creator
     const handler = createHandler<Context, DIDProviderMethods>(didMethods)
-    this._handle = async (msg) => await handler({ did, secretKey }, msg)
+    this._handle = async (msg) => await handler({ did, privateKey }, msg)
   }
 
   get isDidProvider(): boolean {
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,1 +1,26 @@
 // create file like https://github.com/bshambaugh/key-did-provider-ed25519/blob/master/test/index.test.ts
+//import { P256Provider } from '../src/index.js'
+
+describe('key-did-provider-p256', () => {
+
+    it('authenticates correctly', async () => {
+
+    })
+
+    it('signs JWS properly', async () => {
+
+    })
+
+    // this will be able to change when did-jwt supports ES256 for JWE 
+     // https://github.com/decentralized-identity/did-jwt/issues/225
+    /*
+    it('decrypts JWE properly', async () => {
+
+    })
+
+    it('thows if fails to decrypt JWE', async () => {
+   
+    })
+    */
+
+})
